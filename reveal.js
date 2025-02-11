ddad655f2975521b8c4d8e6effac3d91b61e07f8
Lettycia Vitória <163659294+LettyciaDev@users.noreply.github.com>
2025-02-11 20:09:28 -0300

    Site criado

---
+++ reveal.js
@@ -0,0 +1,44 @@
+window.exibir = ScrollReveal({reset:true})
+
+// TOPO DO SITE
+exibir.reveal('.efeito-content-topo',
+{
+  duration: 2000,
+  distance: '30px'
+})
+
+exibir.reveal('.efeito-content-curious1',
+{
+    duration: 2000,
+    distance: '30px'
+})
+
+exibir.reveal('.efeito-content-curious2',
+{
+   duration: 2000,
+   distance: '30px'
+})
+
+exibir.reveal('.efeito-content-curious3',
+{
+       duration: 2000,
+       distance: '30px'
+})
+
+exibir.reveal('.efeito-content-curious4',
+{
+        duration: 2000,
+        distance: '30px'
+})
+
+exibir.reveal('.efeito-content-engenharia',
+{
+           duration: 2000,
+               distance: '30px'
+})
+
+exibir.reveal('.efeito-content-sobre',
+{
+   duration: 2000,
+   distance: '30px'
+})
